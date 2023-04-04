6fe6d161be8ce87c48f9a44466cad6c69f4d4232
JStudio11sys <126428980+JStudio11sys@users.noreply.github.com>
2023-04-04 08:11:58 +0800

    Add files via upload

---
+++ JAVASCRIPT/js/index.js
@@ -0,0 +1,11 @@
+// javascipt exercise 
+
+var button = document.querySelector('button');
+var box = document.getElementById('shape');
+
+function changeColor() {
+    box.style.borderTopRightRadius =  "50%";
+    box.style.borderTopLeftRadius =  "2%";
+    box.style.borderBottomRightRadius =  "30%";
+    box.style.borderBottomLeftRadius =  "50%";
+}
